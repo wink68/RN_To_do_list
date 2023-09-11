850e840011ed97d1a7c91a166f9581b3a5fa561c
김창민 <cosihh55@gmail.com>
2023-09-11 11:40:18 +0900

    feat: to do list 추가

--- App.js
+++ App.js
@@ -1,11 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { theme } from "./colors";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const STORAGE_KEY = "@toDos";
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({});
+
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+
+  const onChangeText = ({ nativeEvent: { text }}) => setText(text);
+
+  // to do list 가져오기
+  const loadToDos = async (toSave) => {
+    const saved = await AsyncStorage.getItem(STORAGE_KEY);
+    // setToDos(JSON.parse(saved))
+    saved !== null ? setToDos(JSON.parse(saved)) : null;
+  };
+
+  useEffect(() => {
+    loadToDos();
+  }, []);
+
+  // to do list 추가
+  const addToDo = async () => {
+    if (text === "") {
+      return;
+    }
+    const newToDos = {
+      ...toDos, [ Date.now() ]: { text, working },
+    };
+    setToDos(newToDos);
+    await saveToDos(newToDos);
+    setText("");
+  };
+
+  // to do list를 스토리지에 저장
+  const saveToDos = async (toSave) => {
+    const stringToDo = JSON.stringify(toSave);
+    await AsyncStorage.setItem(STORAGE_KEY, stringToDo)
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      {/* 헤더 */}
+      <View style={styles.header}>
+        <TouchableOpacity onPress={work}>
+          <Text style={{ ...styles.btnText, color: working ? theme.white : theme.grey }}>Work</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={travel}>
+          <Text style={{ ...styles.btnText, color: !working ? theme.white : theme.grey }}>Travel</Text>
+        </TouchableOpacity>
+      </View>
+      <TextInput
+        style={styles.input}
+        placeholder={working ? "Add a To Do" : "Where do you want to go?"}
+        onChange={onChangeText}
+        onSubmitEditing={addToDo}
+        returnKeyType="done"
+        autoCorrect={true} // 자동완성
+      />
+      <ScrollView>
+        {Object.keys(toDos).map((key) =>
+          toDos[key].working === working ? (
+            <View style={styles.toDo} key={key}>
+              <Text style={styles.toDoText}>{toDos[key].text}</Text>
+            </View>
+          ) : null
+        )}
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +82,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 20,
+    backgroundColor: theme.bg,
   },
+  header: {
+    marginTop: 100,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  btnText: {
+    color: theme.white,
+    fontSize: 38,
+    fontWeight: "600",
+  },
+  input: {
+    marginVertical: 20,
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    backgroundColor: theme.white,
+    borderRadius: 30,
+    fontSize: 16,
+  },
+  toDo: {
+    marginBottom: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    backgroundColor: theme.grey,
+    borderRadius: 15,
+  },
+  toDoText: {
+    color: theme.white,
+    fontSize: 16,
+    fontWeight: "500",
+  }
 });
---
+++ colors.js
@@ -0,0 +1,6 @@
+export const theme = {
+  bg: "#000",
+  grey: "#3A3D40",
+  white: "#fff",
+  toDoBg: "#5C5C60",
+};
